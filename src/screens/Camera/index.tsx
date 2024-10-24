f03252ca59b576a99658c64ec2bb43d5c27e0397
Shamir Duran <shamirduran15@gmail.com>
2024-10-24 11:28:51 -0500

    Zoom and exposure camera controls

--- src/screens/Camera/index.tsx
+++ src/screens/Camera/index.tsx
@@ -1,15 +1,14 @@
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {useContext} from 'react';
 import {Alert, StyleSheet, Text} from 'react-native';
-import {Gesture, GestureDetector} from 'react-native-gesture-handler';
 import Reanimated, {
-  Extrapolation,
   interpolate,
   useAnimatedProps,
+  useDerivedValue,
   useSharedValue,
 } from 'react-native-reanimated';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import {Camera, CameraProps} from 'react-native-vision-camera';
+import {Camera} from 'react-native-vision-camera';
 import {PermissionRequest, VideoModal} from '../../components';
 import {CameraContext} from '../../context';
 import {RootStackParamsList} from '../../navigator/StackNavigator';
@@ -17,7 +16,7 @@ import {BottomControls} from './BottomControls';
 import {UpperControls} from './UpperControls';
 
 Reanimated.addWhitelistedNativeProps({
-  zoom: true,
+  exposure: true,
 });
 
 const ReanimatedCamera = Reanimated.createAnimatedComponent(Camera);
@@ -39,26 +38,22 @@ export const CameraScreen = ({
     requestStoragePermissions,
   } = useContext(CameraContext);
 
-  const zoom = useSharedValue(device!.neutralZoom);
+  const exposureSlider = useSharedValue(0);
+  const exposureValue = useDerivedValue(() => {
+    if (device == null) return 0;
 
-  const zoomOffset = useSharedValue(0);
-  const zoomGesture = Gesture.Pinch()
-    .onBegin(() => {
-      zoomOffset.value = zoom.value;
-    })
-    .onUpdate(event => {
-      const z = zoomOffset.value * event.scale;
-      zoom.value = interpolate(
-        z,
-        [1, 10],
-        [device!.minZoom, device!.maxZoom],
-        Extrapolation.CLAMP,
-      );
-    });
+    return interpolate(
+      exposureSlider.value,
+      [-1, 0, 1],
+      [device.minExposure, 0, device.maxExposure],
+    );
+  }, [exposureSlider, device]);
 
-  const animatedProps = useAnimatedProps<CameraProps>(
-    () => ({zoom: zoom.value}),
-    [zoom],
+  const exporuseanimatedProps = useAnimatedProps(
+    () => ({
+      exposure: exposureValue.value,
+    }),
+    [exposureValue],
   );
 
   if (!hasCameraPermission) {
@@ -116,17 +111,17 @@ export const CameraScreen = ({
   return (
     <SafeAreaView style={styles.container}>
       <UpperControls closeCamera={closeCamera} />
-      <GestureDetector gesture={zoomGesture}>
-        <ReanimatedCamera
-          style={{flex: 1}}
-          device={device}
-          isActive={true} // Start the camera
-          ref={cameraRef}
-          video={true} // Record video
-          audio={true} // Record audio
-          animatedProps={animatedProps}
-        />
-      </GestureDetector>
+      <ReanimatedCamera
+        style={{flex: 1}}
+        device={device}
+        isActive={true} // Start the camera
+        ref={cameraRef}
+        video={true} // Record video
+        audio={true} // Record audio
+        animatedProps={exporuseanimatedProps}
+        enableZoomGesture
+        outputOrientation="device"
+      />
       <BottomControls />
 
       {recordedVideo && (
